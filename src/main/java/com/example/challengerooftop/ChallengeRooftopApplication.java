7ad0856349bffb5db19ebad109dfee48a95881dd
mateo <mateoenriquevillanueva@gmail.com>
2022-03-07 19:43:21 -0300

    first commit

---
+++ src/main/java/com/example/challengerooftop/ChallengeRooftopApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengerooftop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeRooftopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeRooftopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/challengerooftop/ChallengeRooftopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengerooftop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeRooftopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
